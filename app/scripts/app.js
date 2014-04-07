5c43dbb0a94d9078f2c8bfdf6173eff6b9c7886e
Daniel Mendez <dmendez4@its.jnj.com>
2014-04-07 12:14:53 -0400

    Changes app script to support multiple folders per file type

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,10 +5,10 @@ window.Todos = Ember.Application.create({
 });
 
 /* Order and include as you please. */
-require('scripts/controllers/*');
+require('scripts/controllers/{,*/}*');
 require('scripts/store');
-require('scripts/models/*');
-require('scripts/routes/*');
-require('scripts/helpers/*');
-require('scripts/views/*');
+require('scripts/models/{,*/}*');
+require('scripts/routes/{,*/}*');
+require('scripts/helpers/{,*/}*');
+require('scripts/views/{,*/}*');
 require('scripts/router');
